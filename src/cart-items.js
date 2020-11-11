c6da8d6fee83e574dd95e293361ac9d7fdf06298
Tapan Anand <70485285+tapananand95@users.noreply.github.com>
2020-11-11 11:10:38 +0530

    Add files via upload

---
+++ src/cart-items.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    title: "Lenovo Z6 Pro",
+    price: 26999,
+    img: require('./images/z6pro.png'), 
+    amount: 1
+  },
+  {
+    id: 2,
+    title: "Redmi Note 9 Pro",
+    price: 19759,
+    img: require('./images/note9pro.png'),
+    amount: 1
+  },
+  {
+    id: 3,
+    title: "OnePlus8 Glacial Green",
+    price: 49999,
+    img: require('./images/oneplus8.png'),
+    amount: 1
+  },
+  {
+    id: 4,
+    title: "Micromax IN Note 1",
+    price: 10999,
+    img: require('./images/innote1.jpeg'),
+    amount: 1
+  },
+  {
+    id: 5,
+    title: "Asus ROG Phone 3",
+    price: 49999,
+    img: require('./images/rogphone3.jpeg'),
+    amount: 1
+  }
+];
---
+++ src/components/CartContainer.js
@@ -0,0 +1,54 @@
+import React from "react";
+import CartItem from "./CartItem";
+import { connect } from "react-redux";
+import { CLEAR_CART, GET_TOTALS } from "../actions";
+const CartContainer = ({ cart = [], total, dispatch }) => {
+  React.useEffect(() => {
+    dispatch({ type: GET_TOTALS });
+  }, [cart, dispatch]);
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        {/* cart header */}
+        <header>
+          <h2>Your Shopping Cart</h2>
+          <h4 className="empty-cart">is now empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <section className="cart">
+      {/* cart header */}
+      <header>
+        <h2>Your Shopping Cart</h2>
+      </header>
+      {/* cart items */}
+      <article>
+        {cart.map(item => {
+          return <CartItem key={item.id} {...item} />;
+        })}
+      </article>
+      {/* cart footer */}
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>₹{total}</span>
+          </h4>
+        </div>
+        <button
+          className="btn clear-btn"
+          onClick={() => dispatch({ type: CLEAR_CART })}>
+          Clear The Cart
+        </button>
+      </footer>
+    </section>
+  );
+};
+
+function mapStateToProps(store) {
+  const { cart, total } = store;
+  return { cart, total };
+}
+export default connect(mapStateToProps)(CartContainer);
